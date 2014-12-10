bb6170a4241bd61afc670c80ea79d47bfb43f774
ShrenikDaga <shrenikdaga@gmail.com>
2014-12-10 19:15:00 +0530

    Entity Class Stub

--- System/IM/IM.Misc/Entities.cs
+++ System/IM/IM.Misc/Entities.cs
@@ -4,9 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//
+//
+//
+
 namespace IM.Misc
 {
-    public class Class1
+    // 
+    // 
+    // 
+    public class Entities
     {
+
     }
 }
